38936810010dc2d0f9cb695f3e8c014516555de1
heyeuu <2829004293@qq.com>
2025-07-13 23:27:32 +0800

    add env variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,7 +15,12 @@ impl Config {
 
         let query = args.get(1).expect("parse the query failed").clone();
         let file_path = args.get(2).expect("parse the file path failed").clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 
     pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
@@ -31,11 +37,28 @@ impl Config {
     pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         let contents = fs::read_to_string(config.file_path)?;
 
-        for line in Self::search("heyeuuu", &contents) {
+        let result = if config.ignore_case {
+            Self::search_case_insensitive(&config.query, &contents)
+        } else {
+            Self::search(&config.query, &contents)
+        };
+
+        for line in result {
             println!("{line}");
         }
 
         Ok(())
+    }
+
+    pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+        let mut result = Vec::new();
+        let query = query.to_lowercase();
+        for line in contents.lines() {
+            if line.to_lowercase().contains(&query) {
+                result.push(line);
+            }
+        }
+        result
     }
 }
 
@@ -49,6 +72,38 @@ mod tests {
         let contents = "/ hello world!
 rust
         hahhh I'm readyyyyy";
-        assert_eq!(vec!["rust"], Config::search(query, contents));
+        assert_eq!(vec!["rust",], Config::search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "heyeuuu";
+        let contents = "\
+        Hahhhh
+this is a test of rust
+written by 
+heyeuuu
+instead of heyeu
+and
+Heyeuuu";
+        assert_eq!(vec!["heyeuuu"], Config::search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RuSt";
+        let contents = "\
+        hello~
+        I am a girl who want to pursur her happiness and freedom
+and I 
+wanna to study
+rust and prepare a surprise for
+myself!
+let's rusty";
+
+        assert_eq!(
+            vec!["rust and prepare a surprise for", "let's rusty"],
+            Config::search_case_insensitive(query, contents)
+        );
     }
 }
